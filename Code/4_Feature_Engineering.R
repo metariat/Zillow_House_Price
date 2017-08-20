a5a438eb4bef372f4adbd7880afd78a6914945fc
Metariat <quangdx91@gmail.com>
2017-08-20 18:03:42 +0200

    create 4_Feature_Engineering

---
+++ Code/4_Feature_Engineering.R
@@ -0,0 +1,14 @@
+# A lot of additional variables can be found here:
+#   https://www.kaggle.com/nikunjm88/creating-additional-features
+
+
+
+#---------------------
+#-- Altitude
+#---------------------
+#Please refer to the code 2_altitude_calculation.R
+
+#---------------------
+#-- Calculate the distance to the beach
+#---------------------
+#Please refer to the code 3_EDA_water_distance_calculation.R
