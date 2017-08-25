0c2e90abe75ac7e084cd64ebb682e50dcdf459bf
metariat <quangdx91@gmail.com>
2017-08-25 17:58:47 +0200

    v2

--- Code/4_Data_Preparation.R
+++ Code/4_Data_Preparation.R
@@ -10,6 +10,12 @@
 #Run the code number 1, 2, 3 before
 properties = readRDS("C:/Quang/Kaggle/Zillow_House_Price_Data/properties_v2.RDS")
 
+properties[, fips := ifelse(is.na(fips), "6037", as.character(fips))]
+
+del.col = c("tract.block", "regionidzip", "regionidcity", "censustractandblock")
+properties[ ,(del.col) := NULL]
+
+
 
 missing.values <- transpose(data.frame(sapply(properties, function(x) sum(is.na(x))/length(x))))
 colnames(missing.values) = colnames(properties)
@@ -19,5 +25,6 @@ missing.values %>%
   ggplot(aes(x=reorder(feature,missing_pct),y=missing_pct)) +
   geom_bar(stat="identity",fill="red") +
   coord_flip()+theme_bw()
+
 
 
