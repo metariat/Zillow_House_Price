65022ba8080ac4355ab50b9d0f0966869e60afc7
Metariat <quangdx91@gmail.com>
2017-09-03 18:31:23 +0200

    init

---
+++ Code/0_Data_Loading_For_Modelling.R
@@ -0,0 +1,86 @@
+library(data.table)
+gc()
+
+#-------------------------------------------------------------------#
+
+# Solution without outliers capping = 0.2 & transformed variables
+
+#-------------------------------------------------------------------#
+
+
+#   1. DATA Loading
+#---------------------------------
+
+
+path = "C:/documents/xq.do/Desktop/Kaggle/Zillow_House_Price_Data/"
+
+train = setDT(readRDS(paste0(path, "train.RDS")))
+test = setDT(readRDS(paste0(path, "test.RDS")))
+properties = setDT(data.frame(readRDS(paste0(path, "properties_v2_sparse_matrix.RDS"))))
+parcelid = properties$parcelid
+
+#renaming
+setnames(train, old = "N.prop.typeNot Built", new = "N.prop.typeNot.Built")
+setnames(test, old = "N.prop.typeNot Built", new = "N.prop.typeNot.Built")
+setnames(properties, old = "X.Intercept.", new = "(Intercept)")
+
+
+train[, transactiondate := NULL]
+test[, transactiondate := NULL]
+train[, tract.number := NULL]
+test[, tract.number := NULL]
+properties[, tract.number := NULL]
+gc()
+
+
+
+# 2. Transforming high skew variable
+#------------------------------------
+
+skew.var = c("basementsqft",
+             "bathroomcnt",
+             "calculatedbathnbr",
+             "finishedfloor1squarefeet",
+             "calculatedfinishedsquarefeet",
+             "finishedsquarefeet12",
+             "finishedsquarefeet13",
+             "finishedsquarefeet15",
+             "finishedsquarefeet50",
+             "finishedsquarefeet6",
+             "fireplacecnt",
+             "fullbathcnt",
+             "garagecarcnt",
+             "lotsizesquarefeet",
+             "poolsizesum",
+             "roomcnt",
+             "unitcnt",
+             "yardbuildingsqft17",
+             "yardbuildingsqft26",
+             "structuretaxvaluedollarcnt",
+             "taxvaluedollarcnt",
+             "landtaxvaluedollarcnt",
+             "taxamount",
+             "N.living.area.error",
+             "N.living.area.prop",
+             "N.LivingAreaProp2",
+             "N.ExtraSpace",
+             "N.TotalRooms",
+             "N.ValueProp",
+             "N.value.ratio",
+             "N.tax.score",
+             "N.life.tax",
+             "N.zip.count",
+             "N.city.count",
+             "N.tract.count",
+             "water.distance")
+
+gc()
+
+for (i in skew.var){
+  min.va = min(min(properties[, get(i)]), 0)
+  train[, eval(i) := log(get(i) - min.va + 1)]
+  test[, eval(i) := log(get(i) - min.va + 1)]
+  properties[, eval(i) := log(get(i) - min.va + 1)]
+}
+
+gc()
