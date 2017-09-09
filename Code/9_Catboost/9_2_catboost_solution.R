4df240b55e626456aa4228efcb94f252efee1af9
Metariat <quangdx91@gmail.com>
2017-09-09 20:38:28 +0200

    Kaggle submitted solution

--- Code/9_Catboost/9_2_catboost_solution.R
+++ Code/9_Catboost/9_2_catboost_solution.R
@@ -20,11 +20,12 @@ train = train[abs(train$logerror) < 0.2, ]
 y.train = train$logerror
 x.train = subset(train, select= -c(logerror, parcelid))
 
+test = test[abs(test$logerror) < 0.2, ]
 y.test = test$logerror
 x.test = subset(test, select= -c(logerror, parcelid))
 
-
-
+y = c(y.train, y.test)
+x = rbind(x.train, x.test)
 
 #-------------------------------------------------------------------#
 
@@ -43,7 +44,7 @@ maeSummary <- function (train,
 }
 
 control <- trainControl(method = "cv",
-                        number = 3,
+                        number = 5,
                         verboseIter=TRUE,
                         summaryFunction = maeSummary)
 
@@ -53,48 +54,33 @@ grid <- expand.grid(depth = c(5),
                     l2_leaf_reg = c(1e-3),
                     rsm = c(0.6),
                     border_count = c(64))
-#0.04226789
-cb <- train(y          = y.train,
-            x          = x.train, 
+
+cb <- train(y          = y,
+            x          = x, 
             preProcess = NULL,
             method     = catboost.caret, 
             metric     = "MAE", 
             maximize   = FALSE, 
             tuneGrid   = grid, 
-            trControl  = control
-)
-
-#second round
-# depth learning_rate iterations l2_leaf_reg rsm border_count
-# 7     5          0.01        600       0.001 0.5           32
-
-
-#third round
-# depth learning_rate iterations l2_leaf_reg rsm border_count
-# 10     5          0.05        600       0.001 0.5           64
-
-#forth round
-# depth learning_rate iterations l2_leaf_reg rsm border_count
-# 4     5          0.07        600       0.001 0.7           64
-
-# 5 round
-# depth learning_rate iterations l2_leaf_reg rsm border_count
-# 3     5          0.06        600       0.001 0.6           64
-
+            trControl  = control)
 
 
 #Model information
 print(cb)
+#0.0419751
 
-#variable importance
-importance <- varImp(cb, scale = FALSE)
-print(importance)
-plot(importance)
+#Prediction
+cat.pred <- 0.97*predict(cb, properties) + 0.03*0.11
+cat.pred = round(cat.pred, 5)
 
-#training mae
-cat.pred <- predict(cb, x.train)
-mae(cat.pred, y.train)
-
-#testing mae
-cat.pred <- predict(cb, x.test)
-mae(cat.pred, y.test)
+submission = data.frame("parcelid" = as.integer(as.character(properties$parcelid)), 
+                        "a_201610" = cat.pred,
+                        "a_201611" = cat.pred,
+                        "a_201612" = cat.pred,
+                        "a_201710" = cat.pred,
+                        "a_201711" = cat.pred,
+                        "a_201712" = cat.pred)
+submission = setDT(submission)
+setnames(submission, old = c("a_201610", "a_201611", "a_201612", "a_201710", "a_201711", "a_201712"),
+         new = c("201610", "201611", "201612", "201710", "201711", "201712"))
+fwrite(submission, "C:/documents/xq.do/Desktop/Kaggle/Zillow_House_Price_Data/submission/20170909_Q_Quick_tuned_catboost.csv")
