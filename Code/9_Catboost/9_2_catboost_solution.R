95bb2520d8a67a4bbe66c7405fd90fa7a70b77c2
Metariat <quangdx91@gmail.com>
2017-09-09 19:36:42 +0200

    delete trash file

--- Code/9_Catboost/9_2_catboost_solution.R
+++ Code/9_Catboost/9_2_catboost_solution.R
@@ -9,19 +9,19 @@ library(caret) #for hyperparameter tuning
 
 #---------------------------------------------------------------#
 source("C:/documents/xq.do/Desktop/Kaggle/Zillow_House_Price/Code/0_Data_Loading_For_Modelling.R")
-parcelid = properties$parcelid
+
 
 #-------------------------------------------------------------------#
 
-#                   Defining data set
+#                   Defining training & testing set
 
 #-------------------------------------------------------------------#
-train = train[abs(train$logerror) < 0.2]
-test = test[abs(test$logerror) < 0.2]
+train = train[abs(train$logerror) < 0.2, ]
+y.train = train$logerror
+x.train = subset(train, select= -c(logerror, parcelid))
 
-y = c(train$logerror, test$logerror)
-x = rbind(train, test)
-x = subset(x, select = -c(logerror, parcelid))
+y.test = test$logerror
+x.test = subset(test, select= -c(logerror, parcelid))
 
 
 
@@ -43,29 +43,48 @@ maeSummary <- function (train,
 }
 
 control <- trainControl(method = "cv",
-                        number = 5,
+                        number = 3,
                         verboseIter=TRUE,
-                        summaryFunction = maeSummary
-)
+                        summaryFunction = maeSummary)
 
-grid <- expand.grid(depth = c(6),
-                    learning_rate = c(0.0021),
+grid <- expand.grid(depth = c(5),
+                    learning_rate = c(0.06),
                     iterations = c(600),
                     l2_leaf_reg = c(1e-3),
-                    rsm = c(0.95),
-                    border_count = c(32)
+                    rsm = c(0.6),
+                    border_count = c(64))
+#0.04226789
+cb <- train(y          = y.train,
+            x          = x.train, 
+            preProcess = NULL,
+            method     = catboost.caret, 
+            metric     = "MAE", 
+            maximize   = FALSE, 
+            tuneGrid   = grid, 
+            trControl  = control
 )
 
-cb <- train(y=y,
-            x=x, 
-            preProcess=NULL,
-            method=catboost.caret, 
-            metric = "MAE", 
-            maximize = FALSE, 
-            tuneGrid = grid, 
-            trControl = control
-)
+#second round
+# depth learning_rate iterations l2_leaf_reg rsm border_count
+# 7     5          0.01        600       0.001 0.5           32
 
+
+#third round
+# depth learning_rate iterations l2_leaf_reg rsm border_count
+# 10     5          0.05        600       0.001 0.5           64
+
+#forth round
+# depth learning_rate iterations l2_leaf_reg rsm border_count
+# 4     5          0.07        600       0.001 0.7           64
+
+# 5 round
+# depth learning_rate iterations l2_leaf_reg rsm border_count
+# 3     5          0.06        600       0.001 0.6           64
+
+
+
+#Model information
+print(cb)
 
 #variable importance
 importance <- varImp(cb, scale = FALSE)
@@ -73,15 +92,9 @@ print(importance)
 plot(importance)
 
 #training mae
-cat.pred <- predict(cb, x)
-mae(cat.pred, y)
+cat.pred <- predict(cb, x.train)
+mae(cat.pred, y.train)
 
-#making cat.pred
-cat.pred = predict(cb, properties)
-cat.pred = round(cat.pred, 5)
-result <- data.frame(cbind(parcelid, cat.pred, cat.pred, cat.pred, cat.pred, cat.pred, cat.pred))
-colnames(result) <- c("parcelid","201610","201611","201612","201710","201711","201712")
-result$parcelid = as.integer(as.character(result$parcelid))
-fwrite(result, 
-       file = "C:/documents/xq.do/Desktop/Kaggle/Zillow_House_Price_Data/submission/20170902_Q_init_catboost_w_o_outliers_trans_var.csv", 
-       row.names = FALSE ) 
+#testing mae
+cat.pred <- predict(cb, x.test)
+mae(cat.pred, y.test)
